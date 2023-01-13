b5f60f1ce8ed7cf30203738eec080782d65fa643
JaRodriguez97 <cristi542012@gmail.com>
2023-01-13 16:10:57 -0500

    inicio completo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { NgxSpinnerModule } from "ngx-spinner";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { ContactanosComponent } from "./components/contactanos/contactanos.component";
 import { IndexComponent } from "./components/index/index.component";
 import { NosotrosComponent } from "./components/nosotros/nosotros.component";
 import { ServiciosComponent } from "./components/servicios/servicios.component";
 import { TestimoniosComponent } from "./components/testimonios/testimonios.component";
-import { ContactanosComponent } from "./components/contactanos/contactanos.component";
 import { TrabajosComponent } from "./components/trabajos/trabajos.component";
 
 @NgModule({
@@ -24,6 +25,7 @@ import { TrabajosComponent } from "./components/trabajos/trabajos.component";
     NgxSpinnerModule,
     BrowserModule.withServerTransition({ appId: "serverApp" }),
     AppRoutingModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit, HostListener, Inject } from "@angular/core";
-import { DOCUMENT } from "@angular/common";
-import { ActivatedRoute, Params, Router } from "@angular/router";
-// import Swal from "sweetalert2";
-import { NgxSpinnerService } from "ngx-spinner";
+import { Component, OnInit } from "@angular/core";
+import {
+  faFacebookF,
+  faInstagram,
+  faGoogle,
+} from "@fortawesome/free-brands-svg-icons";
+
 @Component({
   selector: "app-index",
   templateUrl: "./index.component.html",
   styleUrls: ["./index.component.css"],
 })
 export class IndexComponent implements OnInit {
-  // clases: { imgBx: boolean; active: boolean } = { imgBx: true, active: false };
+  faFacebookF = faFacebookF;
+  faInstagram = faInstagram;
+  faGoogle = faGoogle;
 
   constructor() {}
-  ngOnInit() {
-    // setTimeout(() => {
-    //   this.clases.active = true;
-    // }, 2000);
-  }
+  ngOnInit() {}
 }
