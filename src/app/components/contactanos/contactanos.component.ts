12c213b94e175466a5dfeb1ff00e957b90d38124
JaRodriguez97 <cristi542012@gmail.com>
2023-02-03 13:15:06 -0500

    Actualizacion 1.0.1

--- src/app/components/contactanos/contactanos.component.ts
+++ src/app/components/contactanos/contactanos.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import Swal from "sweetalert2";
-import { ContactService } from "@service/Contact/contact.service";
 import { Router } from "@angular/router";
+import { ContactService } from "@service/Contact/contact.service";
+import Swal from "sweetalert2";
 @Component({
   selector: "app-contactanos",
   templateUrl: "./contactanos.component.html",
